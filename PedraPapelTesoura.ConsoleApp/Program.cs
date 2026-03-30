0fb9b5d3798301db97eadd7afb2135f3107fe301
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-03-30 14:43:36 -0300

    first commit

---
+++ PedraPapelTesoura.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PedraPapelTesoura.ConsoleApp;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("=========================================");
+    Console.WriteLine("----------Pedra, Papel e Tesoura---------");
+    Console.WriteLine("=========================================");
+  }
+}
